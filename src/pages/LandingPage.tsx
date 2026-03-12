1ed18b0377a00a653df6f469a34cfa991db76b77
noturnocszapps-coder <noturnocszapps@gmail.com>
2026-03-12 14:40:55 -0300

    Criar LandingPage.tsx

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+export function LandingPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>DriverDash</h1>
+      <p>Assuma o controle total dos seus ganhos no volante.</p>
+
+      <a href="/login">
+        <button style={{marginTop:20,padding:10}}>
+          Entrar
+        </button>
+      </a>
+    </div>
+  );
+}
